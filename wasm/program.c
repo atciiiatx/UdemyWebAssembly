df74f99dd293f48b7c6d3c69aa28c3711b82e038
AT Campbell <at.campbell@clearcorrect.com>
2020-01-31 11:32:05 -0600

    Two-way communication between C and JavaScript

---
+++ wasm/program.c
@@ -0,0 +1,22 @@
+extern void numLog(int n);
+extern void strLog(char *s);
+
+int main()
+{
+	return 42;
+}
+
+int getDoubleNumber(int x)
+{
+	return x * 2;
+}
+
+void testNumLog(int x)
+{
+	numLog(x);
+}
+
+void testStrLog()
+{
+	strLog("Hello from C!");
+}
