2afef74c1bc2caa35accc49f4ac07a07349d9b53
AT Campbell <at.campbell@clearcorrect.com>
2020-02-06 17:32:28 -0600

    Generate circle data in C and send it to JavaScript for drawing.

--- project/lib/canvas.c
+++ project/lib/canvas.c
@@ -1,13 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <emscripten.h>
+
+// Constants
+#define NUM_CIRCLES 20
+
+// Data structure
+struct Circle {
+    int x;
+    int y;
+    int r;
+    int cr;
+    int cg;
+    int cb;
+};
+
+struct Circle circles[NUM_CIRCLES];
+
+// Random number in range
+int getRand(int min, int max) {
+    int range = max - min;
+    int result = min + (int)(((float)rand() / (float)RAND_MAX) * range);
+    return result;
+}
 
 // Init circle data and start render
 int main() {
-    printf("Init circles\n");
-    emscripten_run_script("render()");
+    printf("Generate %d circles\n", NUM_CIRCLES);
+    srand(time(NULL));
+    for (int i = 0; i < NUM_CIRCLES; ++i) {
+        int radius = getRand(5, 150);
+        circles[i].x = getRand(0, 1000) + radius;
+        circles[i].y = getRand(0, 1000) + radius;
+        circles[i].r = radius;
+        circles[i].cr = getRand(55, 255);
+        circles[i].cg = getRand(55, 255);
+        circles[i].cb = getRand(55, 255);
+    }
+
+    EM_ASM({ render($0); }, NUM_CIRCLES);
 }
 
-// Get number of circles to draw
-int getCircles() {
-    return 1;
+// Get circles to draw
+struct Circle* getCircles() {
+    return circles;
+}
+
+int getX(int index) {
+    return circles[index].x;
+}
+
+int getY(int index) {
+    return circles[index].y;
+}
+
+int getRadius(int index) {
+    return circles[index].r;
+}
+
+int getRed(int index) {
+    return circles[index].cr;
+}
+
+int getGreen(int index) {
+    return circles[index].cg;
+}
+
+int getBlue(int index) {
+    return circles[index].cb;
 }
